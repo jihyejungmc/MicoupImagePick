cae07c725a6a8bbbcf6aba776249c390b5205f08
kimkr <dev.kimkr@gmail.com>
2019-07-18 17:43:45 +0900

    log 메서드 테스트로 추가.

--- ios/MicoupImagePicker/Classes/MCPMicoupImagePicker.m
+++ ios/MicoupImagePicker/Classes/MCPMicoupImagePicker.m
@@ -1,5 +1,6 @@
 
 #import "MCPMicoupImagePicker.h"
+#import <React/RCTLog.h>
 
 #import "HTTPAPICommunicator.h"
 #import "NSDictionary+MCPSafeConvert.h"
@@ -11,12 +12,29 @@
 
 @end
 
+- (instancetype)init {
+    self = [super init];
+    return self;
+}
+
 @implementation MCPMicoupImagePicker
 
-- (dispatch_queue_t)methodQueue {
++ (BOOL)requiresMainQueueSetup
+{
+    return YES;
+}
+
+- (dispatch_queue_t)methodQueue
+{
     return dispatch_get_main_queue();
 }
+
 RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(log:(NSString *)text)
+{
+  RCTLogInfo(@"FROM MCPMicoupImagePicker %@", text);
+}
 
 RCT_REMAP_METHOD(showImagePickerWithOptions,
                  options:(NSDictionary *)options
