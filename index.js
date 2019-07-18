ac868613b6884231dcce2cc2f3e8970a7b713ef5
kimkr <dev.kimkr@gmail.com>
2019-07-18 15:29:30 +0900

    index.js에 메서드 선언.

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 
 const { RNMicoupImagePicker } = NativeModules;
 
-export default RNMicoupImagePicker;
+const defaultOptions = {
+  documentNo: -1,
+  cookie: undefined,
+  userAgent: Platform.OS === 'ios' ? 'MissyCouponsTest/1.0.6(197.1)' : 'MissyCoupons/1.0.4(3)',
+  imageCount: 1,
+  spanCount: 3,
+  enableCamera: true
+};
+
+export default {
+  showImagePickerWithOptions(optionArgs) {
+    const options = {
+      ...defaultOptions,
+      ...optionArgs,
+    };
+    return RNMicoupImagePicker.showImagePickerWithOptions(options);
+  }
+};
